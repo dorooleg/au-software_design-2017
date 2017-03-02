5b498cbe21b803ff6160295559281c2b6a3270ec
Mikhail Koltsov <misha@koltsov.su>
2017-03-02 16:09:43 +0300

    Implement Environment

--- cli/src/cli/environment.py
+++ cli/src/cli/environment.py
@@ -1,18 +1,46 @@
 import pathlib 
+import os
+import logging
+
 
 class Environment:
+    """A shell environment, in which commands run.
+
+    It is a collection of pairs <var_name, var_value>
+    plus a current directory.
+    """
 
     def __init__(self):
-        pass
+        """Create an empty environment.
+        """
+        self._var_to_value = dict()
+        self._current_working_directory = pathlib.Path(os.getcwd())
 
     def get_var(self, name):
-        pass
+        """Get variable value by name.
+        """
+        return self._var_to_value.get(name, '')
 
     def set_var(self, name, value):
-        pass
+        """Set variable value.
+        """
+        self._var_to_value[name] = value
 
     def get_cwd(self):
-        pass
+        """Get string representation of current working directory.
+        """
+        return str(self._current_working_directory)
 
     def set_cwd(self, dir_name):
-        pass
+        """Set current working directory.
+
+        Args:
+            dir_name (str): string representation of a new working
+                directory path.
+        """
+        dir_name = pathlib.Path(dir_name)
+        new_path = self._current_working_directory.joinpath(dir_name)
+
+        self._current_working_directory = new_path
+        logging.info('Current working directory changed to {}'.format(new_path))
+
